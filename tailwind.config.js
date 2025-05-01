ccbd4b3f76f12ba0503734fd915c4d72971ba826
TrevorRodrigues <denisrovnanik96@gmail.com>
2025-05-01 11:56:57 +0200

    prvá verzia webu

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      textShadow: {
+        black: "10px 10px 15px rgba(0, 0, 0,)"       
+      },
+      colors: {
+        primary: "#b8860b", 
+      font: {
+        libre: "'Libre Franklin'",  
+    },
+  },
+  plugins: [],
+}
+  }}
